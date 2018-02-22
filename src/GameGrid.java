0e2fa0b280df0e559f80eebde6c34d14a25e86bf
Zander <31848787+Zanalm@users.noreply.github.com>
2018-02-22 16:31:06 +0100

    Fixing the graphics...
    Added cases for the steering

--- src/GameGrid.java
+++ src/GameGrid.java
@@ -23,5 +23,74 @@ public class GameGrid {
 		createFood();
 	}
 
-	
+	private boolean isMoveValid(Direction direction) {
+		int headX = snake.getHead().getX();
+		int headY = snake.getHead().getY();
+		switch (direction) {
+		case UP:
+			headY--;
+			break;
+		case RIGHT:
+			headX++;
+			break;
+		case DOWN:
+			headY++;
+			break;
+		case LEFT:
+			headX--;
+			break;
+		}
+		if (headX < 0 || headX >= width || headY < 0 || headY >= height)
+			return false;
+		if (isCovered[headX][headY] == true)
+			return false;
+		isCovered[headX][headY] = true;
+		return true;
+	}
+
+	public void changeDirection(Direction newDirection) {
+		if (snakeDirection.compatibleWith(newDirection)) {
+			snakeDirection = newDirection;
+			isDirectionChanged = true;
+		}
+	}
+
+	private DrawSnake initSnake() { // initial Snake has 3 TypeLinked
+		snake = new DrawSnake();
+		for (int i = 0; i < 3; i++) {
+			snake.addTail(new TypeLinked(i + width / 2, height / 2));
+			isCovered[i + width / 2][height / 2] = true;
+		}
+		return snake;
+	}
+
+	public TypeLinked createFood() {
+		int x, y;
+		do {
+			x = new Random().nextInt(width);
+			y = new Random().nextInt(height);
+		} while (isCovered[x][y] == true);
+		food = new TypeLinked(x, y);
+		return food;
+	}
+
+	public DrawSnake getSnake() {
+		return snake;
+	}
+
+	public TypeLinked getFood() {
+		return food;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public int getScore() {
+		return scores;
+	}
 }
