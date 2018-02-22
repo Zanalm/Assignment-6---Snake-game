ed2bb60207fd2ac87ddb2bef7f273c7d185ebd50
Zander <31848787+Zanalm@users.noreply.github.com>
2018-02-22 16:32:53 +0100

    Setting up key events
    setting up the structure for the events

--- src/KeyAction.java
+++ src/KeyAction.java
@@ -1,4 +1,22 @@
 
 public class KeyAction {
+	private boolean running;
+	private final GameGrid grid;
+	private final UserInterface gameView;
+	
+	public KeyAction(GameGrid grid, UserInterface gameView) {
+		this.grid = grid;
+		this.gameView = gameView;
+		this.running = true;
+	}
+	
+	
+	
+	
+	public void keyReleased(KeyEvent e) {
+	}
+
+	public void keyTyped(KeyEvent e) {
+	}
 
 }
